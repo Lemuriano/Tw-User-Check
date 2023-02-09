2ca46416e24f2983c5309cb1fac2c5f0adf9f8b9
Lemuriano <bernabegoitia@gmail.com>
2023-02-09 09:16:58 -0300

    Primer commit

---
+++ src/config/twitter.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'CzGW1Q2wdyZoKYNePMJKVDWr8',
+    consumer_secret: 'EaAJoMAZDYfrIRdCkSl1KvrzRa7WpXLxWRceaA4M6VflbzMn9S',
+    access_token: '1356944185274863616-gGIocQJi6F5UnoJYSlTbYSbQ9j8ezZ',
+    access_token_secret: 'e2UUq0W5wAEWOB9ZJW45S8CXyx8PHxYtc8jpXldFoCWM2',
+  };
---
+++ src/routes/tweet.js
@@ -0,0 +1,28 @@
+const Twit = require('twit');
+const config = require('../config/twitter');
+
+const T = new Twit(config);
+
+const checkAccountStatus = (req, res) => {
+  const { username } = req.params;
+
+  T.get(`users/show`, { screen_name: username }, (err, data, response) => {
+    console.log(data.status)
+    if (data.status){
+        return res.json({ status: 'cuenta activa' })
+    }else{
+        switch (err.code) {
+            case 50:
+                return res.json({ status: 'Usuario no encontrado' })
+            case 63:
+                return res.json({ status: 'Suspendido' });
+            default:
+                return res.json({ message: 'error en la solicitud' });
+        }
+    }  
+  });
+};
+
+module.exports = {
+  checkAccountStatus,
+};
