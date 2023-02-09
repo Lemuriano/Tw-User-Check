404d41e794b4edc208138ab2d5a7875fa7c1af86
Lemuriano <bernabegoitia@gmail.com>
2023-02-09 09:53:14 -0300

    Commit de configuracion para cyclic

--- src/app.js
+++ src/app.js
@@ -5,6 +5,4 @@ const app = express();
 
 app.use('/tweet/:username', tweetRoutes.checkAccountStatus);
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000.');
-});
+module.exports = app
